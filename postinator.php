7635c115c6c4418c20cf7204e179105d58a322f2
Tim van Bergenhenegouwen <tim@slurfhondje.nl>
2015-03-09 22:17:15 +0100

    Plugin meta

---
+++ postinator.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: Postinator
+Plugin URI: https://github.com/zomnium/postinator
+Version: 0.0.1
+Author: Zomnium
+Author URI: http://zomnium.com/
+Description: Content widgets created for developers, easy for the users.
+*/
