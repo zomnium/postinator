2c3645e4ef74d26f8697fc133fdd8b4dcc45e4ea
Tim van Bergenhenegouwen <tim@slurfhondje.nl>
2015-03-09 22:23:59 +0100

    Class and initialization

--- postinator.php
+++ postinator.php
@@ -1,4 +1,5 @@
 <?php
+
 /*
 Plugin Name: Postinator
 Plugin URI: https://github.com/zomnium/postinator
@@ -7,3 +8,24 @@ Author: Zomnium
 Author URI: http://zomnium.com/
 Description: Content widgets created for developers, easy for the users.
 */
+
+class Postinator extends WP_Widget {
+
+	/**
+	 * Register widget
+	 */
+
+	function __construct() {
+		parent::__construct(
+			'postinator', // Base ID
+			__( 'Show content', 'postinator'), // Name
+			array( 'description' => __( 'Pick me! I show content.', 'postinator' ), ) // Args
+		);
+	}
+}
+
+function postinatorInit() {
+	register_widget( 'Postinator' );
+}
+
+add_action( 'widgets_init', 'postinatorInit' );
