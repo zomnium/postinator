f90fbe5773e031d93f0fe6205b3822dd78401969
Tim van Bergenhenegouwen <tim@slurfhondje.nl>
2015-03-16 20:27:14 +0100

    Title field

--- postinator.php
+++ postinator.php
@@ -60,6 +60,16 @@ class Postinator extends WP_Widget {
 
 	public function form( $instance )
 	{
+		// Fields
+		$title = ( isset( $instance[ 'title'] ) ) ? $instance[ 'title' ] : '';
+
+		// Form
+		?>
+		<p>
+			<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title' ); ?></label>
+			<input type="text" class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" value="<?php echo esc_attr( $title ); ?>" />
+		</p>
+		<?php
 	}
 
 	/**
@@ -68,6 +78,9 @@ class Postinator extends WP_Widget {
 
 	public function update( $new_instance, $old_instance )
 	{
+		$instance = array();
+		$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
+		return $instance;
 	}
 
 	// content (get/fields):
